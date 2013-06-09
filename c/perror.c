dd4c953f10672aed6d56746de2651f4a9a11ef5b
weli <weli@li390-88.members.linode.com>
2013-06-09 18:46:53 +0000

    more fun

--- c/perror.c
+++ c/perror.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int main() {
-  int fd = open('/tmp/void');
-  printf("result %d\n", fd);
+  int fd = open("/tmp/void", O_RDONLY | O_CREAT);
+  perror("result");
   return 0;
 }
