f0cdedc5e26bdbb0ca612275c19a54f0761dd2ff
Weinan Li <weli@redhat.com>
2014-02-25 01:53:05 +0800

    .

--- rmi/RMIDemoClient.java
+++ rmi/RMIDemoClient.java
@@ -6,11 +6,9 @@ import java.rmi.Naming;
 public class RMIDemoClient {
 
     public static void main(String[] args) throws Exception {
-        if (args.length == 2) {
-            String url = new String("rmi://" + args[0] + "/RMIDemo");
-            RMIDemo rmiDemo = (RMIDemo) Naming.lookup(url);
-            String serverReply = rmiDemo.doCommunicate(args[1]);
-            System.out.println("Server Reply: " + serverReply);
-        }
+        String url = new String("rmi://localhost/RMIDemo");
+        RMIDemo rmiDemo = (RMIDemo) Naming.lookup(url);
+        String serverReply = rmiDemo.doCommunicate("Apple");
+        System.out.println("Server Reply: " + serverReply);
     }
 }
