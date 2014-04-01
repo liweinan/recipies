4f31923976ce399b669d862375efa4b8db6e4f3e
Weinan Li <l.weinan@gmail.com>
2014-04-01 13:01:19 +0800

    aaa.asm

--- asm/aaa.asm
+++ asm/aaa.asm
@@ -3,11 +3,12 @@ section .bss
 section .data
 
 section .text
-global main 
-main:
+global _start 
+_start:
 	xor eax, eax
 	mov al, '8' ; 0011 1000 high nybble(0011) is ignored by aaa
 	add al, '2' ; 0110 0010 high nybble(0110) is ignored by aaa
+	; result = 6a
 	aaa ; ah = 0000 0001 (1) al = 0000 0000 (0)
 	
 	mov eax, 1 ; sys_exit
